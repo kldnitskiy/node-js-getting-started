0c1fe4c5c94d76c995423d41d6f29c08811ac3ad
Danl Kladnitskiy <core2pixel@gmail.com>
2020-03-31 16:18:42 +0300

    changes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# node-js-getting-started
+# ITMO megabattle Chatbot
 
 A barebones Node.js app using [Express 4](http://expressjs.com/).
 
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const {Botact} = require('botact')
 const bodyParser = require('body-parser')
 const mysql = require('mysql')
 const server = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 80
 const bot = new Botact({
     token: '91c66aec637d3f1ea0615132ea568793ffc55b18c0dd878c386584ee226bf7264f669f30cc61986c8e3e2',
     confirmation: 'f5f07863'
@@ -14,7 +14,7 @@ bot.on(function (user_message){
     user_message.reply('Привет')
 })
 bot.event('group_join', ({ reply }) => {
-  reply('Большое спасибо, что вступил в группу Мегабаттла! ')
+  reply('Добро пожаловать в семью!Ты сделал маленький, но весомый шаг к незабываемой студенческой жизни♥Это особенное место с неповторимыми людьми✨ А ты уже - наша часть 😌Мы обещаем, будет интересно 😏 ')
 })
 server.use(bodyParser.json())
 
@@ -23,23 +23,23 @@ server.post('/', bot.listen)
 server.get('/', (request, response) => {
     request.header('Content-Type', 'application/json')
     response.send('f5f07863')
-    
-    
 })
-
+var os = require("os");
+var hostname = os.hostname();
+console.log(hostname);
 server.listen(PORT)
 
-let con = mysql.createConnection({
-  host: "us-cdbr-iron-east-01.cleardb.net",
-  user: "b09805f711cdac",
-  password: "c362ba82",
-database: "heroku_2cf38b0299dd81c"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+//let con = mysql.createConnection({
+//  host: "us-cdbr-iron-east-01.cleardb.net",
+//  user: "b09805f711cdac",
+//  password: "c362ba82",
+//database: "heroku_2cf38b0299dd81c"
+//});
+//
+//con.connect(function(err) {
+//  if (err) throw err;
+//  console.log("Connected!");
+//});
 
 bot.command('start', (ctx) => {
   // via shortcut from context
@@ -70,10 +70,10 @@ bot.command('start', (ctx) => {
   bot.reply([ ctx.user_id, 1 ], 'Hi, this is start!')
 })
 
-let sql = "INSERT INTO chatbot_data (vk_id, pair_id) VALUES (3, 4)";
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("1 record inserted");
-  });
+//let sql = "INSERT INTO chatbot_data (vk_id, pair_id) VALUES (3, 4)";
+//  con.query(sql, function (err, result) {
+//    if (err) throw err;
+//    console.log("1 record inserted");
+//  });
 
 //mysql://b09805f711cdac:c362ba82@us-cdbr-iron-east-01.cleardb.net/heroku_2cf38b0299dd81c?reconnect=true
