070bcbb2cc1bb373ca69560ffe134a75faea2954
Danl Kladnitskiy <core2pixel@gmail.com>
2020-03-31 20:46:06 +0300

    mysql

--- index.js
+++ index.js
@@ -24,9 +24,6 @@ server.get('/', (request, response) => {
     request.header('Content-Type', 'application/json')
     response.send('f5f07863')
 })
-var os = require("os");
-var hostname = os.hostname();
-console.log(hostname);
 server.listen(PORT)
 
 let con = mysql.createConnection({
@@ -41,39 +38,11 @@ con.connect(function(err) {
   console.log("Connected!");
 });
 
-bot.command('start', (ctx) => {
-  // via shortcut from context
-  ctx.reply('Hi, this is start!')
-  // via shortcut with keyboard
-  ctx.reply('Yo, this is keyboard?', null, {
-    one_time: false,
-    buttons: [
-      [
-        {
-          action: {
-            type: 'text',
-            payload: {
-              button: 'Hello, world!'
-            },
-            label: 'Hello, world!'
-          },
-          color: 'primary'
-        }
-      ]
-    ]
-  })
-  // via function from context
-  ctx.sendMessage(ctx.user_id, 'Hi, this is start!')
-  // via function from instance
-  bot.reply(ctx.user_id, 'Hi, this is start!')
-  // to multiple users
-  bot.reply([ ctx.user_id, 1 ], 'Hi, this is start!')
-})
-
 let sql = "INSERT INTO chatbot_data (vk_id, pair_id) VALUES (3, 4)";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("1 record inserted");
   });
 
+console.log(sql);
 //mysql://b09805f711cdac:c362ba82@us-cdbr-iron-east-01.cleardb.net/heroku_2cf38b0299dd81c?reconnect=true
