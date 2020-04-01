298eb37bf91b101bfed767d5cfebf5d7ebfeee5d
kladnitskiy <31546027+kldnitskiy@users.noreply.github.com>
2020-04-01 11:09:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -26,23 +26,5 @@ server.get('/', (request, response) => {
 })
 server.listen(PORT)
 
-let con = mysql.createConnection({
-  host: "us-cdbr-iron-east-01.cleardb.net",
-  user: "b09805f711cdac",
-  password: "c362ba82",
-database: "heroku_2cf38b0299dd81c"
-});
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-
-let sql = "INSERT INTO chatbot_data (vk_id, pair_id) VALUES (3, 4)";
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("1 record inserted");
-  });
-
-console.log(sql);
 //mysql://b09805f711cdac:c362ba82@us-cdbr-iron-east-01.cleardb.net/heroku_2cf38b0299dd81c?reconnect=true
